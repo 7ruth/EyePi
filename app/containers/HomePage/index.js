af8467e1a30846c8442df6040bcce8152015bd75
Martin Terskin <martinas.terskin@gmail.com>
2017-09-22 21:40:40 -0400

    final commit

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,6 +9,7 @@ import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import ReactTooltip from 'react-tooltip';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 
@@ -28,6 +29,10 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
     this.props.onPageLoadGetEpisodes();
   }
 
+  componentDidUpdate() {
+    ReactTooltip.rebuild();
+  }
+
   render() {
     const { loading, error, episodes, maxNumVotes } = this.props;
     const IndicatorListProps = {
@@ -38,13 +43,38 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
     const episodeTableProps = episodes;
 
     const columns = [{
+      expander: true,
+      width: 40,
+      Expander: ({ isExpanded, ...rest }) => // eslint-disable-line
+        <div>
+          {isExpanded
+            ? <span
+              style={{
+                color: 'red',
+              }}
+            >&#x2299;</span>
+            : <span
+              style={{
+                color: 'green',
+              }}
+            >&#x2295;</span>}
+        </div>,
+      style: {
+        cursor: 'pointer',
+        fontSize: 25,
+        padding: '0',
+        textAlign: 'center',
+        userSelect: 'none',
+      },
+      Footer: () => <a href="https://i.stack.imgur.com/jiFfM.jpg" style={{ color: 'purple', textDecoration: 'none' }}><span>&reg;</span></a>,
+    }, {
       Header: 'Title',
       accessor: 'originalTitle',
       width: 200,
     }, {
       Header: 'Season',
       accessor: 'seasonNumber',
-      width: 100,
+      width: 80,
       Cell: ({ value }) => (
         <div
           style={{
@@ -81,7 +111,7 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
     }, {
       Header: 'Episode #',
       accessor: 'episodeNumber',
-      width: 100,
+      width: 80,
       Cell: ({ value }) => (
         <div
           style={{
@@ -96,8 +126,11 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
       accessor: 'averageRating',
       width: 200,
       Cell: (row) => { // eslint-disable-line
+        const dataTip = Math.round(row.original.averageRating / 2) + " Picards" // eslint-disable-line
         return (
-          <div>
+          <div
+            data-tip={dataTip}
+          >
             {[...Array(Math.round(row.original.averageRating / 2))].map((_, i) => { // eslint-disable-line
               return <img key={i} alt="Picard" height={34} src="https://www.disruptorbeam.com/assets/uploaded/news-thumbnails/crew_icon_enterprise_d_picard.png" />;
             })}
@@ -108,28 +141,32 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
       Header: 'Number of Votes',
       accessor: 'numVotes',
       width: 200,
-      Cell: (row) => (
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            backgroundColor: '#dadada',
-            borderRadius: '2px',
-          }}
-        >
+      Cell: (row) => {
+        const dataTip = row.value + " votes" // eslint-disable-line
+        return (
           <div
+            data-tip={dataTip}
             style={{
-              width: `${(row.value / maxNumVotes) * 100}%`,
+              width: '100%',
               height: '100%',
-              backgroundColor: row.value > (maxNumVotes * 0.66) ? '#85cc00' //eslint-disable-line
-                : row.value > (maxNumVotes * 0.33) ? '#ffbf00'
-                : '#ff2e00',
+              backgroundColor: '#dadada',
               borderRadius: '2px',
-              transition: 'all .2s ease-out',
             }}
-          />
-        </div>
-      ),
+          >
+            <div
+              style={{
+                width: `${(row.value / maxNumVotes) * 100}%`,
+                height: '100%',
+                backgroundColor: row.value > (maxNumVotes * 0.66) ? '#85cc00' //eslint-disable-line
+                  : row.value > (maxNumVotes * 0.33) ? '#ffbf00'
+                  : '#ff2e00',
+                borderRadius: '2px',
+                transition: 'all .2s ease-out',
+              }}
+            />
+          </div>
+        );
+      },
     }, {
       Header: 'Runtime (Sort Disabled)',
       accessor: 'runtimeMinutes',
@@ -161,6 +198,7 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
 
     return (
       <article>
+        <ReactTooltip place="top" type="dark" effect="float" />
         <Helmet
           title="Star Trek: The Next Generation"
           meta={[
@@ -185,6 +223,43 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
                 style={{ height: '550px' }}
                 defaultPageSize={30}
                 className="-striped -highlight"
+                SubComponent={(row) => {
+                  let firstWordOfEpisodeName;
+                  if (row.original.originalTitle.indexOf(' ') !== -1) {
+                    firstWordOfEpisodeName = row.original.originalTitle.substring(0, row.original.originalTitle.indexOf(' '));
+                    if (firstWordOfEpisodeName.length < 5) {
+                      // if the first word is too short ex. All as in All Good Things episode name, get first two words instead (necessary pattern for the urls to work)
+                      const secondPosition = row.original.originalTitle.split(' ', 2).join(' ').length;
+                      firstWordOfEpisodeName = row.original.originalTitle.substring(0, secondPosition);
+                      firstWordOfEpisodeName = firstWordOfEpisodeName.slice(0, firstWordOfEpisodeName.indexOf(' ')) + firstWordOfEpisodeName.slice(firstWordOfEpisodeName.indexOf(' ') + 1);
+                    }
+                  } else {
+                    firstWordOfEpisodeName = row.original.originalTitle;
+                  }
+                  // if episode name starts with an "A" article, cut it out
+                  if (firstWordOfEpisodeName.substring(0, 1) === 'A' && firstWordOfEpisodeName.substring(0, 3) !== 'All') {
+                    firstWordOfEpisodeName = firstWordOfEpisodeName.substring(1, firstWordOfEpisodeName.length);
+                  }
+
+                  firstWordOfEpisodeName = firstWordOfEpisodeName.toLowerCase();
+                  const subValueURL = 'http://www.jammersreviews.com/st-tng/s' + row.original.seasonNumber + '/' + firstWordOfEpisodeName + '.php'; // eslint-disable-line
+                  const linkText = row.original.originalTitle + ' Review'; // eslint-disable-line
+
+                  return (
+                    <div
+                      style={{
+                        padding: '10px',
+                        margin: 'auto',
+                      }}
+                    >
+                      <a
+                        href={subValueURL}
+                      >
+                        {linkText}
+                      </a>
+                    </div>
+                  );
+                }}
               />
             }
             <IndicatorList {...IndicatorListProps} />
